5fc7250c8cd9edcdf2d86f3ce4d8ed83f99ba1a4
Arrowing <likenovel@qq.com>
2015-02-15 22:40:54 +0800

    some tools

---
+++ encodings/generatorA2U.js
@@ -0,0 +1,44 @@
+var cheerio = require('cheerio'),
+		fs = require('fs'),
+		filename = 'Unicode_to_GB2312_or_GBK_table.html';
+
+fs.readFile(filename, {flag: 'r'}, function (err, data) {
+  if (err) throw err;
+
+  var $ = cheerio.load(data);
+
+	var trs = $('table').find('tr');
+	var tds, unicode, gb2312, map = [];
+
+	trs.each( function(index, element){
+		tds = $(element).find('td');
+
+		tds.each( function(index2, td){
+			var $td = $(td),
+				bytes;
+
+			if(index2 == 0){
+					unicode = $td.text().substr(2); // U+554A、554A
+			}else{
+					gb2312 = $td.find('small');
+
+					if(gb2312.length > 0){
+
+						gb2312 = gb2312.text().substr(0, 4); // 0XB0A1、B0A1、啊
+						gb2312 = parseInt(gb2312, 16); //45217（10进制）
+
+						map[+('0x'+unicode) + index2 - 2] = gb2312;
+					};
+			};
+
+		} );
+
+	} );
+
+	var writableStream = fs.createWriteStream('unicode2ansi_arr.js');
+	writableStream.write(JSON.stringify(map));
+	writableStream.end();
+
+});
+
+
